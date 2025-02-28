1d6e7b899a3025b4a428ec0b7f38a2755117622b
wysocki-bu <wysockip@bu.edu>
2025-02-28 11:13:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("# Simple StreamLit App: Graph Sales")
+st.write("# Simple StreamLit App: Graph of Global Net Income")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Americas NetIncome", "Europe Net Income", "Asia Net Income"])
 
 st.bar_chart(chart_data)
-
-# Create a sample dataframe
-#data = pd.DataFrame({
-#  'Year': [2018, 2019, 2020, 2021],
-#  'Sales': [350, 480, 550, 680]
-#})
- 
-# Create a line chart
-#st.line_chart(data)
