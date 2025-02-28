ee5be44d05a5bc1eced2ea13eca85c9ddd090d7c
wysocki-bu <wysockip@bu.edu>
2025-02-28 11:04:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
- 
+
+
+st.write("# Simple StreamLit App: Graph Sales")
+
 # Create a sample dataframe
 data = pd.DataFrame({
   'Year': [2018, 2019, 2020, 2021],
