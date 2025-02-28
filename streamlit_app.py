3e1d6d3b793f379be000dc75bf325e694dd66349
wysocki-bu <wysockip@bu.edu>
2025-02-28 11:25:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Title for graph
 st.write("# Simple StreamLit App: Graph of Global Net Income")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["Americas Net Income", "Europe Net Income", "Asia Net Income"])
