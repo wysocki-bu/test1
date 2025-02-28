36bdb2912a6c9ec39b901188def468a99a645657
wysocki-bu <wysockip@bu.edu>
2025-02-28 11:08:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,15 @@ import pandas as pd
 
 st.write("# Simple StreamLit App: Graph Sales")
 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
 # Create a sample dataframe
-data = pd.DataFrame({
+#data = pd.DataFrame({
 #  'Year': [2018, 2019, 2020, 2021],
-  'Sales': [350, 480, 550, 680]
-})
+#  'Sales': [350, 480, 550, 680]
+#})
  
 # Create a line chart
-st.line_chart(data)
+#st.line_chart(data)
